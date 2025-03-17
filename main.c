969e7f01925beb035f466d20f09bcbd767071e48
Michele <engmichelerodrigues@gmail.com>
2025-03-17 09:54:07 -0300

    descricao das funcoes

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "projeto.h"
+#include "stdio.h"
+
+int main(){
+ListaDeTarefas lt;
+char arquivo[] = "tarefas";
+int codigo, opcao;
+
+do{
+exibeMenu();
+scanf("%d", &opcao);
+if(opcao == 0){}
+else if(opcao == 1){
+    codigo=criarTarefa(&lt);
+    printf("codigo = %d\n", codigo);
+}
+else if(opcao == 2){
+    codigo=deletarTarefa(&lt);
+    printf("codigo = %d\n", codigo);
+}
+else if(opcao == 3){
+    codigo=listarTarefas(lt);
+    printf("codigo = %d\n", codigo);
+}
+else {
+	printf("opcao invalida\n");
+}
+}while (opcao != 0);
+//return 0;
+//system ("pause");
+
+}
---
+++ projeto.c
@@ -0,0 +1,27 @@
+#include "projeto.h"
+#include "stdio.h"
+
+int criarTarefa(ListaDeTarefas *lt){
+    printf("criar tarefa\n");
+    return 0;
+}
+int deletarTarefa(ListaDeTarefas *lt){
+    printf("deletar tarefa\n");
+    return 0;
+}
+int listarTarefas(ListaDeTarefas lt){
+    printf("listar tarefa\n");
+    return 0;
+}
+
+int carregarTarefas(ListaDeTarefas *lt, char *nome){
+    return 0;
+}
+
+int salvarTarefas(ListaDeTarefas *lt, char *nome){
+    return 0;
+}
+
+void exibeMenu(){
+    printf("menu\n");
+}
